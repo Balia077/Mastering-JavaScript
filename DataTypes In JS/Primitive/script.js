7f3c3d570268abefec921975f8e415e10ea01683
Balaram Das <balaramd894@gmail.com>
2024-12-29 19:08:14 +0530

    Add files via upload

---
+++ DataTypes In JS/Primitive/script.js
@@ -0,0 +1,45 @@
+/* let num = 20;       //Integer number
+console.log(num);   //Output: 20
+
+let number = 20.123;    //Floating point number
+console.log(number);    //Output: 20.123
+
+let marks;
+console.log(marks);     //Output: undefined
+
+let num1 = null;
+console.log(num1);      //Output: null
+
+let num2 = 1314154121321155555555555555555555555555145155225485211225854554455121222677896232212;
+console.log(num2);     //Output: 1.3141541213211556e+99
+
+let num3 = true;
+console.log(typeof(num3));  //Output: boolean
+
+let num4 = "Hello";
+console.log(typeof(num4));  */ //Output: string
+
+/* let integerNumber = 10;
+console.log(typeof(integerNumber)); //Output: number
+
+let floatNumber = 3.14;
+console.log(typeof(floatNumber));   //Output: number
+
+let output = integerNumber + floatNumber;
+console.log(output); */    //Output: 13.14 Integer + Float = Float.
+
+/* let number1 = 13;
+let number2 = 10;
+
+let sum = number1 + number2;
+console.log(sum);               //output: 23
+
+let product = number1 * number2;
+console.log(product);           //output: 130
+
+let quotient = number1 / number2;
+console.log(quotient);  */          //output: 1.3
+
+
+
+
