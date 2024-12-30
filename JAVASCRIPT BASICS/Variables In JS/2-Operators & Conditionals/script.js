13ccbc4b6ecb800c44cec8fb8055816e063eedd2
Balia077 <balaramd894@gmail.com>
2024-12-30 13:03:09 +0530

    Add examples for unary and assignment operators in script.js

--- JAVASCRIPT BASICS/Variables In JS/2-Operators & Conditionals/script.js
+++ JAVASCRIPT BASICS/Variables In JS/2-Operators & Conditionals/script.js
@@ -41,3 +41,28 @@ console.log(b); //9
 
 console.log(a**b); //794280046581
 
+//Unary Operators
+//1. Pre-increment[First increment then use]
+let x = 10;
+console.log(++x);   //11
+console.log(x);     //11
+
+//2. Post-increment[First use then increment]
+let y = 10;
+console.log(y++);   //10
+console.log(y);     //11
+
+//3. Pre-decrement[First decrement then use]
+let z = 10;
+console.log(--z);   //9
+console.log(z);     //9
+
+//4. Post-decrement[First use then decrement]
+let w = 10;
+console.log(w--);   //10
+console.log(w);     //9 
+
+____________________________________________________________________________________________________________________
+
+//2. Assignment Operators:
+
