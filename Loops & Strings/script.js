345f0e55f529fc489666c51acd5f4a4c258e1fd9
Balia077 <balaramd894@gmail.com>
2025-01-03 19:47:45 +0530

    Add loop examples and syntax explanation in script.js

---
+++ Loops & Strings/script.js
@@ -0,0 +1,30 @@
+//Loops
+
+//Loops are a way to repeat the same code multiple times. 
+// There are several types of loops in JavaScript, but the most common one is the for loop.
+
+//1. For loop
+//2. While loop
+//3. Do while loop
+
+
+//4. For in loop
+//5. For of loop
+//6. For each loop
+
+
+//1. For loop:
+
+//Syntax:
+
+// for (initialization; condition; increment/decrement) {
+        // code to be executed
+// }
+
+//Example:
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+//Output: 0 1 2 3 4
+
